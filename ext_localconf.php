fdf4e2326efc40473d17ed126c30b9ae42e8703a
kuschmitt <48821904+kuschmitt@users.noreply.github.com>
2025-04-16 15:00:05 +0200

    add site set configuration

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,7 +6,9 @@
  * LICENSE file that was distributed with this source code.
  */
 
+
 defined('TYPO3') or die();
+use \TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 call_user_func(function (): void {
     $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
     $iconRegistry->registerIcon(
@@ -16,8 +18,18 @@ call_user_func(function (): void {
     );
 
 });
-
+ExtensionManagementUtility::addTypoScriptSetup('
+    module.tx_form.settings.yamlConfigurations {
+     1560425499 = EXT:forms2db/Configuration/Yaml/BaseSetup.yaml
+	 1560425500 = EXT:forms2db/Configuration/Yaml/FormEditorSetup.yaml
+    }
+    plugin.tx_form.settings.yamlConfigurations {
+     1560425499 = EXT:forms2db/Configuration/Yaml/BaseSetup.yaml
+	 1560425500 = EXT:forms2db/Configuration/Yaml/FormEditorSetup.yaml
+    }
+');
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\TYPO3\CMS\Scheduler\Task\TableGarbageCollectionTask::class]['options']['tables']['tx_forms2db_domain_model_mail'] = [
     'dateField' => 'tstamp',
     'expirePeriod' => '180',
  ];
+
